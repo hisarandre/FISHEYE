19aeb1afe9c1153dea8164164c8c0b2edcb57d20
Sara Andre <hisarandre@gmail.com>
2022-07-28 01:00:01 +0900

    Get json data and display photographers

---
+++ scripts/api/api.js
@@ -0,0 +1,14 @@
+async function getData() {
+  return fetch("../../data/photographers.json")
+    .then(function (res) {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then(function (res) {
+      return res.photographers;
+    })
+    .catch(function (err) {
+      console.log("an error occurs", err);
+    });
+}
--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,35 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+  const { name, id, city, country, tagline, price, portrait } = data;
+  const picture = `/assets/photographers/low-res/${portrait}`;
 
-    const picture = `assets/photographers/${portrait}`;
+  function getUserCardDOM() {
+    const article = document.createElement("article");
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    img.setAttribute("alt", "image du photographe " + name);
+
+    const photographerName = document.createElement("h2");
+    photographerName.textContent = name;
+
+    const location = document.createElement("h3");
+    location.textContent = city + ", " + country;
+
+    const tag = document.createElement("p");
+    tag.textContent = tagline;
+
+    const pricePerDay = document.createElement("span");
+    pricePerDay.classList.add("price");
+    pricePerDay.textContent = price + "€/jour";
+
+    article.appendChild(img);
+    article.appendChild(photographerName);
+    article.appendChild(location);
+    article.appendChild(tag);
+    article.appendChild(pricePerDay);
+
+    return article;
+  }
+
+  return { name, id, city, country, tagline, price, picture, getUserCardDOM };
 }
